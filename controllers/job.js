3ae0b15da57eb2937fdcb9dd7fcfa2857cc942f7
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-05-09 10:56:16 -0700

    all jobs that match user id func is working

--- controllers/job.js
+++ controllers/job.js
@@ -1,6 +1,26 @@
 const connection = require('../config/db.connection');
 const db = require('../models');
 
+// NOTE Index
+const index = async (req, res) => {
+  const user_id = req.user_id;
+  try {
+    const jobs = await db.Job.findAll({ where: { user_id_fk: user_id } });
+    if (jobs) {
+      res.json({
+        status: 200,
+        data: jobs,
+        requestedAt: new Date().toLocaleString(),
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Something went wrong! Please try again',
+    });
+  }
+};
+
 // NOTE Create
 const createJob = async (req, res) => {
   const user_id = req.user_id;
@@ -62,5 +82,6 @@ const createJob = async (req, res) => {
 };
 
 module.exports = {
+  index,
   createJob,
 };
--- routes/job.js
+++ routes/job.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 const authRequired = require('../middleware/authRequired');
 
+router.get('/', authRequired, ctrl.job.index);
 router.post('/newjob', authRequired, ctrl.job.createJob);
 
 module.exports = router;
