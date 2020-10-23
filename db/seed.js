5354f39d935fca6fc2d879220628b0adcc32c0eb
ADamian17 <adonisdesuvikdo@gmail.com>
2020-10-23 15:06:50 -0700

    added try catch in seed file

--- db/seed.js
+++ db/seed.js
@@ -5,42 +5,51 @@ const jobs = require('./jobs.json');
 
 const seedDB = async () => {
 
-    await User.deleteMany({});
-
-    let password = user.password;
-    const salt = await bcrypt.genSaltSync(10);
-    const hash = await bcrypt.hashSync( password, salt );
-    password = hash;
-
-    const newUser = {
-        first_name: user.first_name, 
-        last_name: user.last_name, 
-        email: user.email,
-        password: password, 
-        profession: user.profession
-    }
-
-    const createdUser = await User.create( newUser );
-
-    const newJobs = jobs.map( job => {
-        if ( job.user_id === null ) {
-            job.user_id = createdUser._id
-        }
+    try {
         
-        return job
-    });
+        await User.deleteMany({});
 
-    await Job.deleteMany({})
+        let password = user.password;
+        const salt = await bcrypt.genSaltSync(10);
+        const hash = await bcrypt.hashSync( password, salt );
+        password = hash;
 
-    for (let job of newJobs ) {
-       const creaatedJob = await Job.create( job )
+        const newUser = {
+            first_name: user.first_name, 
+            last_name: user.last_name, 
+            email: user.email,
+            password: password, 
+            profession: user.profession
+        }
 
-       createdUser.jobs.push(creaatedJob)
-       await createdUser.save()
+        const createdUser = await User.create( newUser );
+
+        const newJobs = jobs.map( job => {
+            if ( job.user_id === null ) {
+                job.user_id = createdUser._id
+            }
+            
+            return job
+        });
+
+        await Job.deleteMany({})
+
+        for (let job of newJobs ) {
+        const creaatedJob = await Job.create( job )
+
+        createdUser.jobs.push(creaatedJob)
+        await createdUser.save()
+        }
+
+        console.log( 'createdUser', createdUser );
+
+        process.exit()
+
+    } catch (error) {
+        return console.log(error)
+        process.exit()
     }
 
-    console.log( 'createdUser', createdUser );
-    process.exit()
 };
 
 seedDB();
