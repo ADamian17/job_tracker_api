a6d945e4e543c5b9138d780339f00dededffa9e7
ADamian17 <adonisdesuvikdo@gmail.com>
2021-01-03 20:34:23 -0800

    added check form field middleware

--- middleware/checkFormFields.js
+++ middleware/checkFormFields.js
@@ -1,3 +1,23 @@
 module.exports = ( req, res, next ) => {
 
-}
+    const fields = Object.keys( req.body );
+
+    if ( !fields ) return res.status(400).json({
+        status: 400,
+        message: 'Please complete all fields',
+    });
+
+    for ( let idx = 0; idx < fields.length; idx++ ) {
+
+        const field = fields[idx];
+
+        if ( req.body[field] === '' ) return res.status(400).json({
+            status: 400,
+            fields: req.body,
+            message: 'Please complete all fields',
+        });
+        
+    };
+
+    next();
+};
--- routes/job.js
+++ routes/job.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
-const { authRequired } = require('../middleware');
+const { authRequired, checkFormFields } = require('../middleware');
 
 // IMPORTANT base route /api/v1/jobs
 router.get('/', authRequired, ctrl.job.index);
-router.post('/newjob', authRequired, ctrl.job.createJob);
+router.post('/newjob', authRequired, checkFormFields, ctrl.job.createJob);
 router.get('/:id', authRequired, ctrl.job.showJob);
 router.put('/update/:id', authRequired, ctrl.job.updateJob);
 router.delete('/delete/:id', authRequired, ctrl.job.destroy);
