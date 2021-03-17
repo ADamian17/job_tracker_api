abf81bbb4d88e43a28c2d81094925f7d6d46cba5
ADamian17 <adonisdesuvikdo@gmail.com>
2021-03-16 21:55:41 -0700

    remove idex users and pw

--- controllers/user.js
+++ controllers/user.js
@@ -1,108 +1,85 @@
 // NOTE internal modules
-const { User, Job } = require('../models');
-const { populate } = require('../models/Job');
-
-// NOTE All users
-const index = async (req, res) => {
-    try {
-        const users = await User.find({});
-
-        res.json({
-            status: 200,
-            data: users,
-            requestedAt: new Date().toLocaleString(),
-        });
-
-    } catch (err) {
-        return res.status(500).json({
-        status: 500,
-        message: 'Something went wrong! Please try again',
-        });
-    }
-};
+const { User, Job } = require("../models");
 
 // NOTE Profile
 const profile = async (req, res) => {
   const userId = req.user;
 
-    try {
-        const user = await User.findById( userId )
-        .populate('jobs')
+  try {
+    const user = await User.findById(userId).populate("jobs");
 
-        res.status(200).json({ 
-            status: 200,
-            data: user 
-        });
-
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'Something went wrong. Please try again',
-        });
-    };
+    res.status(200).json({
+      status: 200,
+      data: user,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again",
+    });
+  }
 };
 
 // NOTE Update user
 const update = async (req, res) => {
-    const userId = req.user;
-    const { first_name, last_name, email, profession } = req.body;
+  const userId = req.user;
+  const { first_name, last_name, email, profession } = req.body;
 
-    const fields = [ first_name, last_name, email ];
+  const fields = [first_name, last_name, email];
 
-    if ( !fields && 
-            first_name === '' || 
-            last_name === '' || 
-            email === '' ||
-            profession === ''
-        ) {
-        return res.status(400).json({
-            status: 400,
-            message: 'Please complete all fields',
-        });
-    }
+  if (
+    (!fields && first_name === "") ||
+    last_name === "" ||
+    email === "" ||
+    profession === ""
+  ) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please complete all fields",
+    });
+  }
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate( userId, req.body, { new: true });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
 
-        return res.status(200).json({
-            status: 200,
-            data: updatedUser,
-            message: `Your user have being update it`,
-        });
-      
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'Something went wrong. Please try again',
-        });
-    };
+    return res.status(200).json({
+      status: 200,
+      data: updatedUser,
+      message: `Your user have being update it`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again",
+    });
+  }
 };
 
 // NOTE delete user
 const destroy = async (req, res) => {
-    const userId = req.user;
+  const userId = req.user;
 
-    try {
-        const deletedUser = await User.findByIdAndDelete( userId );
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
 
-        const deletedJobs = await Job.deleteMany( { user_id: userId } )
-        
-        return res.status(200).json({
-            status: 200,
-            data: deletedUser,
-            message: 'success',
-        });
+    const deletedJobs = await Job.deleteMany({ user_id: userId });
 
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'Something went wrong. Please try again',
-        });
-    }
+    return res.status(200).json({
+      status: 200,
+      data: deletedUser,
+      message: "success",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again",
+    });
+  }
 };
 
 module.exports = {
-  index,
   profile,
   update,
   destroy,
