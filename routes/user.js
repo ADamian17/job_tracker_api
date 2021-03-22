3a1322665825924a1bfa9e666008e84926d5c1c1
ADamian17 <adonisdesuvikdo@gmail.com>
2021-03-22 12:19:52 -0700

    added addImg ctrl

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const { authRequired, checkFormFields } = require("../middleware");
 // IMPORTANT base route /api/v1/users
 router.get("/profile", authRequired, user.profile);
 router.put("/update", authRequired, checkFormFields, user.update);
+router.put("/addprofileimg", authRequired, checkFormFields, user.addImg);
 router.delete("/delete", authRequired, user.destroy);
 
 module.exports = router;
