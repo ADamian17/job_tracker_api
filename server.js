57bcd5f32f27640d599d3dc0192fa90f0fa0e091
ADamian17 <adonisdesuvikdo@gmail.com>
2021-04-20 10:04:53 -0700

    added new root domain

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const LIMIT = rateLimit({
 
 /* NOTE Cors */
 const corsOption = {
-    origin: [ process.env.REACT_APP_URL, 'http://localhost:3000' ],
+    origin: [ process.env.REACT_APP_URL, process.env.REACT_APP_URL_2, 'http://localhost:3000' ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
