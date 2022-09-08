0550b1b8a7eb4fd76c96de0bdc5babdedf3d8c49
ADamian17 <adonisdesuvikdo@gmail.com>
2022-09-07 22:43:01 -0700

    job routes

--- routes/job.js
+++ routes/job.js
@@ -4,8 +4,7 @@ const ctrl = require('../controllers');
 const { authRequired, checkFormFields } = require('../middleware');
 
 // IMPORTANT base route /api/v1/jobs
-// router.get('/', authRequired, ctrl.job.index);
-router.get('/', ctrl.job.index);
+router.get('/', authRequired, ctrl.job.index);
 router.post('/newjob', authRequired, checkFormFields, ctrl.job.createJob);
 router.get('/:id', authRequired, ctrl.job.showJob);
 router.put('/update/:id', authRequired, checkFormFields, ctrl.job.updateJob);
