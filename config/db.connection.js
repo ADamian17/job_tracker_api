c66bd0a9f64505d8eebb12a044cf595a79b928f3
ADamian17 <adonisdesuvikdo@gmail.com>
2023-07-28 19:39:16 -0700

    upgraded pkg

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-const connectionStr = process.env.MONGODB_URI || "mongodb://localhost:27017/trackthatjob-dev";
+const connectionStr =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/trackthatjob-dev';
 
-mongoose.connect( connectionStr, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
-.then(function () {
-    console.log("Mongodb connected....");
-})
-.catch(function (error) {
-  console.log("Mongodb connection err", error);
-});
-
+mongoose
+  .connect(connectionStr, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(function () {
+    console.log('Mongodb connected....');
+  })
+  .catch(function (error) {
+    console.log('Mongodb connection err', error);
+  });
 
 mongoose.connection.on('disconneted', (error) => {
-  console.log(error)
+  console.log(error);
 });
